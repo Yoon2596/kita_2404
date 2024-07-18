47d37962b9f3b1ee925f39e008bfc5ef61b2dd03
Yoon2596 <167948418+Yoon2596@users.noreply.github.com>
2024-07-18 12:43:15 +0900

    Add files via upload

---
+++ m4_웹어플_구축프로젝트/웹애플리케이션/todoList_07_mysql_old/config.py
@@ -0,0 +1,11 @@
+SECRET_KEY = 'b1910a39bb7ffb5c07ee96823aed828fe8656b4dcc460115'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///tasks.db'
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# import os
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'mysql://kita2:kita2@localhost:3306/kita2_db'
